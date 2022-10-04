e0e9b19f83ed0becab735237e80bc8924848a85a
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-10-04 16:42:30 +0200

    Front Controller

--- header.php
+++ header.php
@@ -6,7 +6,6 @@ include_once 'fonction.php';?>
     <head>
         <meta charset="utf-8">
         <title> <?php echo getTitle();?></title>
-        <?php var_dump($_SERVER);?>
     </head>
     <header>
         <h1> Mon header </h1>
