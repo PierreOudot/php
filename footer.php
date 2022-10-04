620ed75dc589f58b7d6e651bfeccf3ea39a5fa1e
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-10-04 10:24:48 +0200

    header,footer,accueil, notes

---
+++ footer.php
@@ -0,0 +1,4 @@
+    <footer>
+        <h1>mon footer</h1>
+    </footer>
+</html>
---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> header</title>
+    </head>
+    <header>
+        <h1> Mon header </h1>
+    </header>
+
+    
