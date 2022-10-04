8feb65eaefb11433e985945dfaf59c6e580e0346
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-10-04 11:04:44 +0200

    monsitebase

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<head>
+    <meta charset="utf_8">
+    <title>mon footer</title>
+</head> 
     <footer>
         <h1>mon footer</h1>
     </footer>
---
+++ mapage.php
@@ -0,0 +1,23 @@
+<?php require 'header.php';?>
+<head>
+    <meta charset="utf-8">
+    <title>ma page d'accueil</title>
+</head>
+<body>
+    <div class="div_accueil">
+        <p>Bienvenue sur la page d'accueil de mon site: un peu de tout et surtout du rien. Yum-yum.</p>
+
+    </div>
+    </br>
+
+    <h2>Menu</h2>
+    <div class="div_accueil_menu">
+        <ul class="menu">
+            <li><a href="tout.php" target="_blank">Tout</a></li>
+            <li><a href="rien.php" target="_blank">Rien</a></li>
+            <li><a href="tout_et_rien.php" target="_blank">Tout et Rien</a></li>
+        </ul>
+    </div>
+    
+</body>
+<?php require 'footer.php';?>
